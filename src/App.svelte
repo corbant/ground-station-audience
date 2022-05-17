339901938919279341b3c71c818ed4cc747749a2
IIVioletKingII <samdepoule@gmail.com>
2022-05-17 14:08:38 -0700

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,46 @@
+<script>
+
+
+
+
+	const source = new EventSource('/events');
+
+	source.addEventListener('position', function(e) {
+		console.log(e.data);
+	});
+
+	source.addEventListener('altitude', function(e) {
+		console.log(e.data);
+	});
+
+	source.addEventListener('status', function(e) {
+		console.log(e.data);
+	});
+</script>
+
+<main>
+	<h1>Ground Control to Major Tom!</h1>
+	<p>This was served from the ground station right to your phone.</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
